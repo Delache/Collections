9bb1995b9860eead3dcb587b3c0fa3e42d5157cb
Delache <delache33@gmail.com>
2019-11-09 13:31:50 +0100

    inMemoryData

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { PokemonsModule } from './pokemons/pokemons.module';
 
 import { AppComponent } from './app.component';
@@ -11,7 +12,8 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
   imports: [
     BrowserModule,
     PokemonsModule, // L'odre de chargement des modules est très important
-    AppRoutingModule // pour l'ordre de déclaration des routes !
+    AppRoutingModule , // pour l'ordre de déclaration des routes !
+    HttpClientModule
   ],
   declarations: [
     AppComponent,
